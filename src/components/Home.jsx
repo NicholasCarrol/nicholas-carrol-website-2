9fab5d4a682e8ec311256adf83b29a1df15ac4a8
ANC <1writingwarrior@gmail.com>
2025-09-06 10:08:45 -0400

    Add links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ export default function HomeConfig() {
                 <h1 class="text-4xl md:text-6xl font-extrabold text-gray-200 mb-4">Darkness of Creation</h1>
                 <p class="text-lg md:text-xl text-gray-100 font-bold mb-4">“Quillan can do infinitely more than any creator before him.
                     <span class="text-blue-600"> For good.</span> Or for <span class="text-red-700"><i>evil</i>.</span>”</p>
-                <p class="text-lg text-gray-300 mb-1"><i>Coming October 1st 2025</i></p>
+                <p class="text-lg text-gray-300 mb-1"><i>Coming October 17th 2025</i></p>
             </section>
 
             {/* <!-- Book Info --> */}
@@ -28,15 +28,15 @@ export default function HomeConfig() {
                     <p class="text-lg md:text-xl text-gray-700 mb-8">A seasoned mercenary, a sheltered princess, and an orphan
                         creator must work together to stop Quillan, the most powerful creator the world has ever seen. If only
                         each of them didn't have their own secret ties to Quillan.</p>
-                    <a href="https://www.amazon.com/dp/B0FPXT63C8"
+                    <a href="https://www.amazon.com/dp/B0FPXT63C8" target="_blank"
                         class="bg-gray-600 text-white px-6 py-3 rounded-lg font-semibold hover:bg-gray-700 hover:text-red-500 transition">Purchase
                         on
                         Amazon</a>
                 </section>
             </div>
 
-            {/* <!-- ----------------------------Reviews Section--------------------------------- --> */}
-            <section class="bg-gray-300 container mx-auto flex flex-col items-start min-h-[20vh] px-6 py-4 rounded-xl">
+             {/* <!-- ----------------------------Reviews Section--------------------------------- --> */}
+            {/*<section class="bg-gray-300 container mx-auto flex flex-col items-start min-h-[20vh] px-6 py-4 rounded-xl">
                 <div class="w-full mb-6 py-6">
                     <h2 class="text-3xl font-bold text-gray-800 text-left">What People Are Saying</h2>
                 </div>
@@ -61,31 +61,14 @@ export default function HomeConfig() {
                             well-developed and the plot twists kept me on the edge of my seat."</p>
                         <div class="text-sm text-gray-500">- Jane D.</div>
                     </div>
-                    {/* <!-- Add more review cards here if needed --> */}
+                    {/* <!-- Add more review cards here if needed --> 
                 </div>
-            </section>
+            </section> */}
 
             {/* <!----------------News letter Section--------------------------> */}
          <div class="flex w-full my-12 justify-center py-4 ">
           <BeeHiivEmbed embedCode={beehiivFormCode} />
         </div>
-            {/* <section class="bg-gray-100 flex flex-col items-center justify-center min-h-[20vh] text-center px-6 py-8">
-                <h2 class="text-3xl font-bold text-gray-800 mb-4">Stay In Touch!</h2>
-                <p class="text-lg text-gray-700 mb-6">Subscribe to my newsletter to get the latest updates on book
-                    releases, events, and exclusive content.</p>
-                <form class="w-full max-w-md" onSubmit={NewsLetter} >
-                    <div class="flex items-center border-b border-gray-400 py-2">
-                        <input
-                            class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                            type="email" placeholder="Your email address" aria-label="Email address" required></input>
-                        <button
-                            class="flex-shrink-0 bg-gray-600 text-white text-sm px-6 py-2 rounded-lg hover:bg-gray-700 hover:text-red-500 transition"
-                            type="submit">
-                            Subscribe
-                        </button>
-                    </div>
-                </form>
-            </section> */}
         </>
 
     )
